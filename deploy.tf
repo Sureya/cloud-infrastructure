e8530a643d28938c0a9928b59fa779de779c1814
Sureya Sathiamoorthi <sureyasathiamoorthi@Sureyas-MacBook-Pro.local>
2019-01-22 10:46:06 +0000

    initial push

--- deploy.tf
+++ deploy.tf
@@ -1,9 +1,9 @@
 module "deploy-eu-west-2" {
-  source = "deployment\/cloud-infrastructure\/basic"
+  source = "basic"
   region = "eu-west-2"
 }
 
 module "deploy-ap-south-1" {
-  source = "deployment\/cloud-infrastructure\/basic"
+  source = "basic"
   region = "ap-south-1"
 }
